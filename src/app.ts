b054eacb3960db6cde9625ae93d8cd1718165366
Sean Luo <shawnluo415@gmail.com>
2022-05-13 20:33:19 -0700

    updated package.json

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,13 @@ import dotenv from "dotenv";
 import path from "path";
 import http from "http";
 
-import { connectDB } from "./util/database.util";
+import { connectDB } from "./util/database.util.js";
 
-import APIRouter from "./router/api.router";
+import APIRouter from "./router/api.router.js";
 
-import errorHandlingMiddleware from "./middleware/errorHandling.middleware";
+import errorHandlingMiddleware from "./middleware/errorHandling.middleware.js";
 
-import SocketIO from "./util/socketIO.util";
+import SocketIO from "./util/socketIO.util.js";
 
 class App {
   private _app: express.Application;
